fc2f65ad0105b9527a262f75a8d0ed78b15ee72c
geraldgsh <geraldgsh@hotmail.com>
2019-01-13 14:21:46 +0800

    first commit

---
+++ js/controllers/MainController.js
@@ -0,0 +1,76 @@
+app.controller('MainController', ['$scope', function($scope) {
+  $scope.today = new Date();
+
+  $scope.appetizers = [
+    {
+      name: 'Caprese',
+      description: 'Mozzarella, tomatoes, basil, balsmaic glaze.',
+      price: 4.95
+    },
+    {
+      name: 'Mozzarella Sticks',
+      description: 'Served with marinara sauce.',
+      price: 3.95
+    },
+    {
+      name: "Bruschetta",
+      description: "Grilled bread garlic, tomatoes, olive oil.",
+      price: 4.95
+    }
+  ];
+
+  $scope.mains = [
+        {
+      name: "Margherita",
+      description: "Marinara sauce, mozzarella, fresh basil.",
+      price: 6.95
+    },
+    {
+      name: "Pepperoni",
+      description: "Pepperoni, marinara sauce, mozzarella, fresh oregano.",
+      price: 7.95
+    },
+    {
+      name: "Rustica",
+      description: "Marinara sauce, kalamata olives, roasted garlic, fresh oregano.",
+      price: 8.95
+    }
+  ];
+  
+  $scope.extras = [
+    {
+      name: "Breadsticks",
+      description: "Served with marinara sauce.",
+      price: 4.95
+    },
+    {
+      name: "Soup of the Day",
+      description: "Take a chance.",
+      price: 4.95
+    },
+    {
+      name: "Buffalo Wings",
+      description: "Feel the power of wings.",
+      price: 6.95
+    }
+  ];
+  
+    $scope.drinks = [
+    {
+      name: "Aperol Spritz",
+      description: "King Valley Prosecco, Aperol, soda water.",
+      price: 14.95
+    },
+    {
+      name: "Classic Champagne Cocktail",
+      description: "Bollinger Champagne, St. Remy VSOP, Angostura bitters, sugar.",
+      price: 24.95
+    },
+    {
+      name: "Cosmopolitan",
+      description: "Absolut vodka, Cointreau, cranberry juice, lime juice.",
+      price: 21.95
+    }
+  ];
+  
+}]);
